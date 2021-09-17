3ac7418c56aaafeb5ca1096eeb4673f1d2f7b2ad
AdrianVides56 <adrianvj18@gmail.com>
2021-09-17 14:14:28 -0500

    Changed comment style

--- 0x08-palindrome_integer/palindrome.h
+++ 0x08-palindrome_integer/palindrome.h
@@ -1,10 +1,12 @@
 #ifndef PALINDROME_H
 #define PALINDROME_H
 
+/* Include libraries */
 #include <stdlib.h>
 #include <stdio.h>
 
+/* function declaration */
 int is_palindrome(unsigned long n);
 int intLen(unsigned long n);
 
-#endif //PALINDROME_H
+#endif /* PALINDROME_H */

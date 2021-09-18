deac026bcd385eee39387d6f9b3e3f8fd975873c
AdrianVides56 <adrianvj18@gmail.com>
2021-09-18 14:53:06 -0500

    Changed how number was stored

--- 0x08-palindrome_integer/0-is_palindrome.c
+++ 0x08-palindrome_integer/0-is_palindrome.c
@@ -10,8 +10,15 @@ int is_palindrome(unsigned long n)
 	char nstr[intLen(n) + 1];
 	int left = 0, right = intLen(n) - 1;
 
-	snprintf(nstr, sizeof(nstr), "%ld", n); /* sprintf(buffer, "%ld", n); */
+	//snprintf(nstr, sizeof(nstr), "%ld", n); /*sprintf(buffer, "%ld", n);*/
 
+	while (n)
+	{
+		nstr[left++] = n % 10;
+		n /= 10;
+	}
+
+	left = 0;
 	while (left <= right)
 	{
 		if (nstr[left] != nstr[right])

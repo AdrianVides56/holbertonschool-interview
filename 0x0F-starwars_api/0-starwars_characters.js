c479d0f8777c79c767b1ca709d1a86f8d526bd06
AdrianVides56 <adrianvj18@gmail.com>
2021-11-10 10:58:24 -0500

    Printed the names acording to its character id in ascending order

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,20 +2,38 @@
 const request = require('request');
 const movieId = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-const allNames = [];
+const allNames = {};
+let ordered = [];
 
 request.get(url, (err, res, body) => {
-  if (err) { console.error(err); } else {
+  if (err)
+  {
+    console.error(err);
+  } 
+  else
+  {
     const characters = JSON.parse(body).characters;
-    for (let i = 0; i < characters.length; i++) {
+    for (let i = 0; i < characters.length; i++)
+    {
       request.get(characters[i], (err, res, body) => {
-        if (err) { console.error(err); } else {
+        if (err)
+          console.error(err);
+        else
+        {
           const name = JSON.parse(body).name;
-          allNames.push(name);
-          if (i === (characters.length - 1)) {
-            for (let j = 0; j < allNames.length; j++) {
-              console.log(allNames[j]);
+          const key = JSON.parse(body).url.split('/')[5];
+          allNames[key] = name;
+          ordered.push(parseInt(key));
+          if (i === (characters.length - 1))
+          {
+            ordered.sort((a, b) => a - b);
+            for (let j = 0; j < i; j++)
+            {
+              const name = allNames[ordered[j]];
+              ordered[j] = name;
             }
+            for (let j = 0; j < i; j++)
+              console.log(ordered[j]);
           }
         }
       });

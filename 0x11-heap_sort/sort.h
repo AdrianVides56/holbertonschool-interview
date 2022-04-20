79510ae3bc5ede9bbb75aef801fa32962e33dbc8
AdrianVides56 <adrianvj18@gmail.com>
2022-04-20 09:40:16 -0500

    Added task files

---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,12 @@
+#ifndef HEAP_SORT_H
+#define HEAP_SORT_H
+
+/* Libraries */
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Function definitions */
+void print_array(const int *array, size_t size);
+void heap_sort(int *array, size_t size);
+
+#endif /* HEAP_SORT_H */

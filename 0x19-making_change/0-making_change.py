3c1b1ac7e02ea5acc80aebdef565b04e29b0d928
AdrianVides56 <adrianvj18@gmail.com>
2022-06-20 11:30:58 -0500

    Added task files

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Solution of the problem
+"""
+
+from audioop import reverse
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins needed to meet a given amount"""
+    count, aux, i = 0, 0, 0
+
+    coins.sort(reverse=True)
+
+    if total == 0:
+        return count
+
+    while i < len(coins):
+        if coins[i] <= total and coins[i] + aux <= total:
+            count += 1
+            aux += coins[i]
+        else:
+            i += 1
+
+    if aux < total:
+        return -1
+    return count

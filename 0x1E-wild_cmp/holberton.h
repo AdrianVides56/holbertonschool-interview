26bc1a289a046a0cd716646b0b0fba8b6014df85
AdrianVides56 <adrianvj18@gmail.com>
2022-07-25 10:13:12 -0500

    Added task files

---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Libraries */
+#include <stdio.h>
+#include <string.h>
+
+/* Functions */
+int wildcmp(char *s1, char *s2);
+
+#endif /* HOLBERTON_H */

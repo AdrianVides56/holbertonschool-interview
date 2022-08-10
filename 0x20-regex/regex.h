b3b6361b7e4ea6acadde327fe721efb692387cc7
AdrianVides56 <adrianvj18@gmail.com>
2022-08-10 11:14:04 -0500

    Added task files

---
+++ 0x20-regex/regex.h
@@ -0,0 +1,12 @@
+#ifndef REGEX_H
+#define REGEX_H
+
+/* Libraries */
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/* Functions */
+int regex_match(char const *str, char const *pattern);
+
+#endif /* REGEX_H */

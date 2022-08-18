f079f5bdd2539f7b3f88e6438cfdb0278c1f371d
AdrianVides56 <adrianvj18@gmail.com>
2022-08-18 10:12:33 -0500

    Added task files

---
+++ 0x21-substring/substring.h
@@ -0,0 +1,13 @@
+#ifndef SUBSTRING_H
+#define SUBSTRING_H
+
+/* Libraries */
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/* Functions */
+
+int *find_substring(char const *s, char const **words, int nb_words, int *n);
+
+#endif /* SUBSTRING_H */
